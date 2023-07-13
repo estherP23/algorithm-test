b22227e246af4c99f336cf0345607b6712ba3466
estherP23 <123536492+estherP23@users.noreply.github.com>
2023-07-13 17:03:41 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int connect (int a, int b) {
+    int temp = b;
+    while ( temp > 0 ) {
+        a *= 10;
+        temp /= 10;
+    }
+    return a+b;
+}
+
+
+int solution(int a, int b) {
+    int plus = connect(a,b);
+    int multi = 2*a*b;
+    
+  
+    if (plus >= multi) return plus;
+        else  return multi;
+}
